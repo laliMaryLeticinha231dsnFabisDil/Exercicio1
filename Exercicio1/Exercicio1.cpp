cf17751241ef84865cf8d7dc92d360c7af626be8
Letícia Fariasf 2b <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2024-04-12 15:52:50 -0300

    Initial commit

---
+++ Exercicio1/Exercicio1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+//PRIMEIRO EXERCÍCIO
+/*using namespace std;
+int main() {
+	int instalacao = 300;
+	int limpeza = 150;
+	double manutencao = 499.99;
+float soma = instalacao +  limpeza + manutencao;
+cout <<soma;
+	return 0;
+}*/
+
+//SEGUNDO EXERCICIO
+/*using namespace std;
+int main() {
+	int horas = 8;
+	int salarioHora = 35;
+
+	int mult = horas * salarioHora;
+	cout << mult;
+	return 0;
+}*/
+
+//TERCEIRO EXERCICIO
+/*using namespace std;
+int main() {
+	int bombom = 16;
+	int caixaDEbombom = 27;
+
+	int sub =  caixaDEbombom - bombom;
+	cout << sub;
+	return 0;
+}*/
+
+
+//QUARTO EXERCICIO
+/*using namespace std;
+int main() {
+	int CaixaDeGarrafaDeAgua = 20;
+	int GarrafasVendidas = 300;
+
+	int div = GarrafasVendidas / CaixaDeGarrafaDeAgua;
+	cout << div;
+	return 0;
+}*/
+
+//QUINTO EXERCICIO
+/*using namespace std;
+int main() {
+	int CamisetaVendida = 80;
+	int Custo = 30;
+
+	int mult = CamisetaVendida * Custo;
+	cout << mult;
+	return 0;
+}*/
